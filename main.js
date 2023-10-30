00751fbcda9bd4b8b3bbdb1f916899a5c57c06fb
romillan <romillan2196@gmail.com>
2023-10-30 15:16:30 -0300

    2da entrega

--- main.js
+++ main.js
@@ -1,72 +1,35 @@
-//para comentar "//"
-/* y 
-asi
-para 
-en
-bloque
-*/
-
-/*
-let edad =5; //variable de tipo number
-let nombre= "andres"; //una variable de tipo string
-
-//forma no recomendada para declarar variables
-var direccion = "AV";
-
-const IVA = 0.21;
-
-nombre = "juan";
-
-console.log (direccion + " " + nombre)
-*/
-
-
-/*
-let numeroUno = 5;
-let numeroDos = 10;
-
-let resultadoSuma = numeroUno + numeroDos;
-let resultadoResta = numeroUno - numeroDos;
-let resultadoMultiplicacion = numeroUno * numeroDos;
-let resultadoDivision = numeroUno / numeroDos;
-
-console.log("Resultado suma", resultadoSuma);
-console.log("Resultado resta", resultadoResta);
-console.log("Resultado multiplicacion", resultadoMultiplicacion);
-console.log("Resultado division", resultadoDivision);
-*/
-
-/*let saludo ="Hola"
-let nombre ="Pepito"
-
-let saludoCompleto = saludo + " " + nombre;
-console.log(saludoCompleto);
-console.log(saludo);
-console.log(nombre);
-*/
-
-/*let numero = Number (prompt("Ingrese un numero"))
-alert(numero + 4)
-*/
-
-let nombre = prompt ("Ingrese nombre ")
-alert("Hola"+ " " + nombre)
-
-let mail = prompt ("Ingrese mail para recibir las ultimas ofertas ")
-alert("El mail:"+ " " + mail+" "+"ha sido ingresado correctamente" )
-
- let edad = prompt ("Ingrese edad ")
-
- if (edad >= 18) {
-   alert("Para pagos con tarjeta presentar C.I.")
-
- } else {
-alert("Solo compras en el local acompanado de un mayor");
-
+function Producto(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
 }
 
 let carrito = [];
 let totalCarrito = 0;
+
+function agregarAlCarrito(producto) {
+    carrito.push(producto);
+    totalCarrito += producto.precio;
+    alert(`${producto.nombre} ha sido agregado al carrito.`);
+}
+
+function mostrarCarrito() {
+    if (carrito.length === 0) {
+        alert('El carrito está vacío.');
+    } else {
+        let productosEnCarrito = carrito.map(producto => producto.nombre);
+        alert(`Carrito:
+        Productos: ${productosEnCarrito.join(', ')}
+        Total: $${totalCarrito}`);
+    }
+}
+
+carrito.buscarProducto = function(nombreProducto) {
+    return this.find(producto => producto.nombre === nombreProducto);
+};
+
+carrito.filtrarPorPrecio = function(precioMaximo) {
+    return this.filter(producto => producto.precio <= precioMaximo);
+};
 
 while (true) {
     let opcion = prompt(`Seleccione una opción:
@@ -74,6 +37,8 @@ while (true) {
     2. Cortapelos
     3. Rasuradoras
     4. Mostrar carrito
+    5. Buscar producto por nombre
+    6. Filtrar productos por precio
     Escriba "salir" para salir.`);
 
     if (opcion === 'salir') {
@@ -83,77 +48,83 @@ while (true) {
     switch (opcion) {
         case '1':
             let submenuPatilleras = '';
-            while (submenuPatilleras !== 'menu') {
+            while (submenuPatilleras !== 'salir') {
                 submenuPatilleras = prompt(`Submenu Patilleras:
                 1. KEMEII - $50
                 2. CRONIER - $40
                 3. SAMSUNG - $30
-                Escriba "menu" para volver al menú principal.`);
+                Escriba "salir" para volver al menú principal.`);
 
                 if (submenuPatilleras === '1') {
-                    carrito.push('KEMEII');
-                    totalCarrito += 50;
+                    agregarAlCarrito(new Producto('KEMEII', 50));
                 } else if (submenuPatilleras === '2') {
-                    carrito.push('CRONIER');
-                    totalCarrito += 40;
+                    agregarAlCarrito(new Producto('CRONIER', 40));
                 } else if (submenuPatilleras === '3') {
-                    carrito.push('SAMSUNG');
-                    totalCarrito += 30;
+                    agregarAlCarrito(new Producto('SAMSUNG', 30));
                 }
             }
             break;
 
         case '2':
             let submenuCortapelos = '';
-            while (submenuCortapelos !== 'menu') {
+            while (submenuCortapelos !== 'salir') {
                 submenuCortapelos = prompt(`Submenu Cortapelos:
                 1. HTC - $60
                 2. GAMA - $45
                 3. WAHL - $55
-                Escriba "menu" para volver al menú principal.`);
+                Escriba "salir" para volver al menú principal.`);
 
                 if (submenuCortapelos === '1') {
-                    carrito.push('HTC');
-                    totalCarrito += 60;
+                    agregarAlCarrito(new Producto('HTC', 60));
                 } else if (submenuCortapelos === '2') {
-                    carrito.push('GAMA');
-                    totalCarrito += 45;
+                    agregarAlCarrito(new Producto('GAMA', 45));
                 } else if (submenuCortapelos === '3') {
-                    carrito.push('WAHL');
-                    totalCarrito += 55;
+                    agregarAlCarrito(new Producto('WAHL', 55));
                 }
             }
             break;
 
         case '3':
             let submenuRasuradoras = '';
-            while (submenuRasuradoras !== 'menu') {
+            while (submenuRasuradoras !== 'salir') {
                 submenuRasuradoras = prompt(`Submenu Rasuradoras:
                 1. KEMEII - $70
                 2. PHILIPS - $65
                 3. VGR - $80
-                Escriba "menu" para volver al menú principal.`);
+                Escriba "salir" para volver al menú principal.`);
 
                 if (submenuRasuradoras === '1') {
-                    carrito.push('KEMEII');
-                    totalCarrito += 70;
+                    agregarAlCarrito(new Producto('KEMEII', 70));
                 } else if (submenuRasuradoras === '2') {
-                    carrito.push('PHILIPS');
-                    totalCarrito += 65;
+                    agregarAlCarrito(new Producto('PHILIPS', 65));
                 } else if (submenuRasuradoras === '3') {
-                    carrito.push('VGR');
-                    totalCarrito += 80;
+                    agregarAlCarrito(new Producto('VGR', 80));
                 }
             }
             break;
 
         case '4':
-            if (carrito.length === 0) {
-                alert('El carrito está vacío.');
+            mostrarCarrito();
+            break;
+
+        case '5':
+            let nombreProducto = prompt('Ingrese el nombre del producto a buscar:');
+            const productoEncontrado = carrito.buscarProducto(nombreProducto);
+            if (productoEncontrado) {
+                alert(`Producto encontrado: ${productoEncontrado.nombre}, Precio: $${productoEncontrado.precio}`);
             } else {
-                alert(`Carrito:
-                Productos: ${carrito.join(', ')}
-                Total: $${totalCarrito}`);
+                alert('Producto no encontrado en el carrito.');
+            }
+            break;
+
+        case '6':
+            let precioMaximo = parseFloat(prompt('Ingrese el precio máximo para filtrar productos:'));
+            const productosFiltrados = carrito.filtrarPorPrecio(precioMaximo);
+            if (productosFiltrados.length > 0) {
+                let productosFiltradosNombres = productosFiltrados.map(producto => producto.nombre);
+                alert(`Productos encontrados: ${productosFiltradosNombres.join(', ')}`);
+            } else {
+                alert('No se encontraron productos que cumplan con el filtro.');
             }
             break;
 
